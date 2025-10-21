e4bfd97942b9135035e5eca34e4848df1bd74e02
rrheon <yongheon0806@gmail.com>
2025-10-21 23:18:54 +0900

    refactor: 나의 경기 관리화면 버튼 스타일 추가

--- TodaysPlayer/TodaysPlayer/Scene/MatchList/SubViews/MatchInfoView.swift
+++ TodaysPlayer/TodaysPlayer/Scene/MatchList/SubViews/MatchInfoView.swift
@@ -16,6 +16,8 @@ struct MatchInfoView: View {
     let postedMatchCase: PostedMatchCase
     let apply: (userId: String, rejectReason: String, status: ApplyStatus)   // 신청자Id: 참여상태
     
+    @Binding var isFinishedMatch: String
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             // 제목
@@ -50,17 +52,20 @@ struct MatchInfoView: View {
             }
             .visible(postedMatchCase == .appliedMatch && matchInfo.organizerId != apply.userId)
             
+            Button("경기 종료하기"){
+                // 서버에 데이터 쏴주고 수정된 데이터로 모집중인 매치, 종료된 매치 수정
+                print("시마이")
+                isFinishedMatch = matchInfo.id
+            }
+            .modifier(MyMatchButtonStyle())
+            .visible(postedMatchCase == .myRecruitingMatch && matchInfo.status != "finished")
             
             NavigationLink {
                 PlayerRatingView(viewModel: PlayerRatingViewModel(matchInfo: matchInfo))
             } label: {
                 Text("참여자 평가하기")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 14)
-                    .background(Color.green)
-                    .cornerRadius(12)
+                    .modifier(MyMatchButtonStyle())
+
             }
             .visible(postedMatchCase == .finishedMatch && matchInfo.organizerId == apply.userId)
         }
